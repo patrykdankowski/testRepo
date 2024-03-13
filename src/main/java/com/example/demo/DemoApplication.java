d771bce89f58df889c12891749caf5bf19cb42d3
patrykdankowski <patryk.dankowski.kontakt@gmail.com>
2024-03-13 20:15:18 +0100

    added for loop

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,14 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+
+        System.out.println("aaa");
+
+        for (int i = 0; i <3; i++) {
+            System.out.println(i);
+        }
+
     }
 
 }
